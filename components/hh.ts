f749d98a740524117a84cb48eaa6c8dda4d17913
Mohammad-Rishad-p <r25richu@gmail.com>
2024-07-23 10:05:28 +0530

    minimum inccrease if the remaining time less than 1 minute

--- components/hh.ts
+++ components/hh.ts
@@ -7,12 +7,10 @@ export async function POST(request: any) {
         const { userName } = await request.json();
         await dbConnect();
         console.log("connected to db in live auction");
-
         const url = new URL(request.url);
         const pathname = url.pathname;
         const productId = pathname.split('/').pop();
         console.log(productId);
-
         const product = await Product.findById(productId);
         console.log("product fetched successfully");
 
@@ -20,20 +18,8 @@ export async function POST(request: any) {
             console.log("No product found in the database.");
             return NextResponse.json({ message: "No product found with the given _id." });
         }
-
-        const currentTime = new Date();
-        const auctionDate = product.auctionDate;
-
-        // Check if the difference is less than one minute and greater than zero seconds
-        const timeDifference = auctionDate.getTime() - currentTime.getTime();
-
-        if (timeDifference < 60000 && timeDifference > 0) {
-            const increaseTime = product.minimumIncrease * 1000
-            product.auctionDate = new Date(auctionDate.getTime() + increaseTime);
-        }
-
-        product.currentBid = product.currentBid + product.minimumIncrease;
-        product.bidWinner = userName;
+        product.currentBid = product.currentBid + product.minimumIncrease
+        product.bidWinner = userName
         await product.save();
 
         return NextResponse.json(product);
